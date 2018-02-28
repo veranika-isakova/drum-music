3c8f7e0507a048aab693ce6c8109f1ba05844562
VERANIKA <veranika.isakova89@gmail.com>
2018-02-28 14:52:06 +0100

    Refactoring

--- music.js
+++ music.js
@@ -1,15 +1,12 @@
-window.addEventListener('keydown', function(e){
-  console.log(e.keyCode);
+function playSound(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  console.log(audio);
   if(!audio) return;
   audio.currentTime = 0;
   audio.play();
-  console.log(key);
   //key.addClass('playing')
   key.classList.add('playing');
-});
+}
 
 function removeTransition (e) {
   if (e.propertyName !== 'transform') return;
@@ -18,3 +15,5 @@ function removeTransition (e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
