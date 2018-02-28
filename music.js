526b013a14e9fd10dadc168e1a645715f1ea1d05
VERANIKA <veranika.isakova89@gmail.com>
2018-02-28 14:20:10 +0100

    Change color after clicking and playing sound

---
+++ music.js
@@ -0,0 +1,12 @@
+window.addEventListener('keydown', function(e){
+  console.log(e.keyCode);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(audio);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  console.log(key);
+  //key.addClass('playing')
+  key.classList.add('playing');
+});
